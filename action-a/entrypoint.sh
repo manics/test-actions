508206e8a631c45ea359f09bdad96fc5d4787f91
Simon Li <orpheus+devel@gmail.com>
2019-06-08 15:08:12 +0100

    Print out vars, and referenced files

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,4 +1,17 @@
-#!/bin/sh -l
+#!/bin/bash -l
 
-env | cut -d= -f1
+VARS=`env | cut -d= -f1`
+for var in $VARS; do
+    if [ -f "$var" ]; then
+        echo "File: $var"
+        echo ====================
+        cat $var
+        echo ====================
+    elif [ "$var" = GITHUB_TOKEN ]; then
+        echo "Variable: GITHUB_TOKEN"
+    else
+        echo "Variable: $var=${!var}"
+    fi
+done
+
 sh -c "echo $*"
