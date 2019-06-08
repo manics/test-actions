740929a3e10737f914c2f908b55d1ca751f95a37
Simon Li <orpheus+devel@gmail.com>
2019-06-08 17:32:04 +0100

    echo git command before running

--- action-a/action.py
+++ action-a/action.py
@@ -4,23 +4,25 @@
 import json
 import os
 from pprint import pprint
-from subprocess import (
-    check_call,
-    check_output
-)
+from subprocess import check_output
 
 
 def get_textfile_at_commit(commit, filepath):
-    ver = check_output(['git', 'show', '{}:{}'.format(commit, filepath)])
-    return ver.decode()
+    return run_git('show', '{}:{}'.format(commit, filepath))
+
+
+def run_git(*args):
+    cmd = ['git'] + list(args)
+    print(' '.join(cmd))
+    out = check_output(cmd)
+    return out.decode()
 
 
 for k in sorted(os.environ):
     if k != 'GITHUB_TOKEN':
         print('{}={}'.format(k, os.getenv(k)))
 
-print(check_output(['git', 'config', '-l']).decode())
-# print(check_output(['git', 'config', '-l', '--global']))
+print(run_git('config', '-l'))
 
 if (os.getenv('GITHUB_EVENT_NAME') == 'push') and (
         os.getenv('GITHUB_REF') == 'refs/heads/master'):
@@ -38,7 +40,7 @@ if (os.getenv('GITHUB_EVENT_NAME') == 'push') and (
         print('{} unchanged'.format(ver_after))
     else:
         print('{} → {}'.format(ver_before, ver_after))
-        check_call(['git', 'tag', ver_after])
-        check_call(['git', 'push', 'origin', 'master'])
+        run_git('tag', ver_after)
+        run_git('push', 'origin', ver_after)
 
-print(check_output(['git', 'tag']).decode())
+print(run_git('tag'))
